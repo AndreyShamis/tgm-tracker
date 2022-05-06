d9494670a574a39fe8df685d87edb3c88f720759
Andrey Shamis <lolnik@gmail.com>
2022-05-06 12:15:19 +0300

    improve code for app
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- track/__main__.py
+++ track/__main__.py
@@ -24,16 +24,16 @@ import logging
 from datetime import datetime
 from sys import argv, exit
 from time import mktime, sleep
-
+import random
 import telethon.sync
 from settings import API_HASH, API_ID
 from telethon import TelegramClient
 from telethon.tl.types import UserStatusOffline, UserStatusOnline
 
-DATETIME_FORMAT = '%Y-%m-%d @ %H:%M:%S'
+DATETIME_FORMAT = '%Y/%m/%d %H:%M:%S'
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(module)14.14s:%(lineno)4.4d | %(levelname)-5.5s  - %(message)s', datefmt='%d/%m/%y %H:%M:%S')
 #logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
@@ -58,55 +58,63 @@ last_offline = None
 last_known_online = None
 
 #bot_entity = client.get_input_entity(peer="ashamis_bot")
-
+logging.info(f' {"-"*25} Start {"-"*25}')
 # logging.info(f'bot_entity:{bot_entity}')
-while True:
-    if contact_id in ['me', 'self']:
-        # Workaround for the regression in Telethon that breaks get_entity('me'):
-        # https://github.com/LonamiWebs/Telethon/issues/1024
-        contact = client.get_me()
-        print(f"here me")
-    else:
-        contact = client.get_entity(contact_id)
-        # results = client.send_message(entity=bot_entity, message='aaaa')
+try:
+    while True:
+        try:
+            if contact_id in ['me', 'self']:
+                # Workaround for the regression in Telethon that breaks get_entity('me'):
+                # https://github.com/LonamiWebs/Telethon/issues/1024
+                contact = client.get_me()
+                print(f"here me")
+            else:
+                contact = client.get_entity(contact_id)
+                # results = client.send_message(entity=bot_entity, message='aaaa')
 
-    if isinstance(contact.status, UserStatusOffline):
-        if online != False:
-            online = False
-            if last_known_online:
-                logging.info(f'~{datetime.now().strftime(DATETIME_FORMAT)}: DISCONNECT. {datetime.now().timestamp()-last_known_online} sec')
+            if isinstance(contact.status, UserStatusOffline):
+                ct = utc2localtime(contact.status.was_online)
+                if online != False:
+                    online = False
+                    kt = f'{ct.strftime(DATETIME_FORMAT)}'
+                    if last_known_online:
+                        logging.info(f'{kt}: DISCONNECT. {round(datetime.now().timestamp()-last_known_online, 3)} sec. [{round(ct.timestamp()-last_known_online, 3)}]')
+                    else:
+                        logging.info(f'{kt}: DISCONNECT.')
+                    last_known_online = None
+                    sleep(2)
+                elif last_offline != contact.status.was_online:
+                    if last_offline is not None:
+                        logging.info(f'{kt}: DISCONNECT after being online for short time!!!!!')
+                    else:
+                        logging.info(f'{kt}: DISCONNECT !!!!.')
+                else:
+                    sleep(5 + random.randint(0, 3))
+                last_offline = contact.status.was_online
+            elif isinstance(contact.status, UserStatusOnline):
+                if online != True:
+                    online = True
+                    last_known_online = datetime.now().timestamp()
+                    logging.warning(f'~{datetime.now().strftime(DATETIME_FORMAT)}: CONNECT.')
+                sleep(3)
+            else:
+                if online != False:
+                    online = False
+                    if last_known_online:
+                        logging.info(f'~{datetime.now().strftime(DATETIME_FORMAT)}: User went offline. {datetime.now().timestamp()-last_known_online}  sec')
+                    else:
+                        logging.info(f'~{datetime.now().strftime(DATETIME_FORMAT)}: User went offline {last_known_online}')
+                    last_known_online = None
                 sleep(2)
-            else:
-                logging.info(f'={utc2localtime(contact.status.was_online).strftime(DATETIME_FORMAT)}: DISCONNECT.')
-                sleep(5)
-            last_known_online = None
+                last_offline = None
+        except Exception as e:
+            logging.exception(e)
+        if online == True:
+            sleep(5 + random.randint(0, 1))
+        else:
+            sleep(5 + random.randint(1, 15))
+except Exception as e:
+    logging.exception(e)
+except KeyboardInterrupt as e:
+    logging.error(e)
 
-        elif last_offline != contact.status.was_online:
-            if last_offline is not None:
-                print(f'={utc2localtime(contact.status.was_online).strftime(DATETIME_FORMAT)}: User went offline after being online for short time.')
-            else:
-                print(f'={utc2localtime(contact.status.was_online).strftime(DATETIME_FORMAT)}: User went offline.')
-        else:
-            sleep(5)
-        last_offline = contact.status.was_online
-    elif isinstance(contact.status, UserStatusOnline):
-        if online != True:
-            online = True
-            logging.warning(f'~{datetime.now().strftime(DATETIME_FORMAT)}: CONNECT.')
-            sleep(5)
-            last_known_online = datetime.now().timestamp()
-        else:
-            sleep(5)
-    else:
-        if online != False:
-            online = False
-            if last_known_online:
-                print(f'~{datetime.now().strftime(DATETIME_FORMAT)}: User went offline. {datetime.now().timestamp()-last_known_online}  sec')
-            else:
-                print(f'~{datetime.now().strftime(DATETIME_FORMAT)}: User went offline {last_known_online}')
-            last_known_online = None
-        else:
-            sleep(2)
-
-        last_offline = None
-    sleep(5)
